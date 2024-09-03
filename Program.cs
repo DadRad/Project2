939648f9bc79efbe2f1d1b4788c868a2d6718c7b
DadRad <126799940+DadRad@users.noreply.github.com>
2024-09-03 18:02:49 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,16 +8,20 @@ class Program
         string name = Console.ReadLine();
         Console.Write("Укажите ваш возраст:");
         int age;
+        
         while (!int.TryParse(Console.ReadLine(), out age))
         { 
             Console.WriteLine("Введите корректный возраст!");
         }
+        
         Console.Write("Укажите ваш опыт работы в нашей сфере:");
         int workExpirience;
+        
         while (!int.TryParse(Console.ReadLine(), out workExpirience))
         {
             Console.WriteLine("Введите корректное число!");
         }
+        
         Console.WriteLine($"Вас зовут {name}, ваш возраст - {age}, ваш опыт работы - {workExpirience}");
     }
 }
