37518351ef336214481e0f48787074b4944593e4
unknown <Dad_Rad04@mail.ru>
2024-09-03 16:06:55 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int userCount = 100;
+        double productPrice = 29.99;
+        string productName = "C# Book";
+        bool isAvailableInStock = true;
+        char firstLetter = 'K';
+        int bossHP = 100;
+        float productRating = 4.5f;
+        long totalSales = 1500000;
+        byte userLevel = 5;
+        string userFeedback = "Отличный продукт!";
+    }
+}
